83bd55bf0f92cc137bb8a0ad60975d380fac215a
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-04-23 17:51:41 +0200

    Base home component working

--- src/app/web/pages/home/home.component.ts
+++ src/app/web/pages/home/home.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: { ngSkipHydration: 'true' },      // Permite la funcion autoPlay del p-gallery funcionar bien
 })
 export class HomeComponent {
-
+  images: any[] = [
+    {
+      itemImageSrc: 'assets/IMG_01.jpg',
+      alt: 'Imagen 1',
+      title: 'Foto 1'
+    },
+    {
+      itemImageSrc: 'assets/IMG_05.jpg',
+      alt: 'Imagen 2',
+      title: 'Foto 2'
+    },
+    {
+      itemImageSrc: 'assets/IMG_09.jpg',
+      alt: 'Imagen 3',
+      title: 'Foto 3'
+    }
+  ];
 }
