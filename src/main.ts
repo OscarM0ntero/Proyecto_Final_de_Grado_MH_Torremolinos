6bab423867caf5174f1c7e6313c2188c1b02ace8
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-04-27 19:11:42 +0200

    Added provider for gallery animation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Importar provideAnimations para p-galleria
 
 platformBrowser().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
+  providers: [provideAnimations()]
 })
   .catch(err => console.error(err));
+
