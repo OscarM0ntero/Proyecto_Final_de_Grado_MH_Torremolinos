b927fd293ccc45120c19401937e4cdcd510072e0
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-08 19:50:15 +0200

    Grid for home imported

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
-//import { BookingComponent } from './pages/booking/booking.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-//import { GalleryComponent } from './pages/gallery/gallery.component';
 import { WebRoutingModule } from './web-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -16,12 +14,12 @@ import { LocationComponent } from './pages/location/location.component';
 import { BookingComponent } from './pages/booking/booking.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { LoginComponent } from './pages/login/login.component';
+import { GridOverlayComponent } from './pages/home/components/grid-overlay/grid-overlay.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    //BookingComponent,
     LayoutComponent,
     ImgTextComponent,
     GalleryComponent,
@@ -29,7 +27,7 @@ import { LoginComponent } from './pages/login/login.component';
     BookingComponent,
     ContactComponent,
     LoginComponent,
-    //GalleryComponent
+    GridOverlayComponent,
   ],
   imports: [
     CommonModule,
