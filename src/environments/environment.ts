12e73f591855725d4ce2dc83187b489e8b9f6ec0
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-11 02:46:59 +0200

    Implemented environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	webUrl: 'http://localhost:4200/',
+	backgroundUrl: 'https://image.tmdb.org/t/p/original',
+	imageUrl: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2',
+	baseUrl: 'http://79.72.63.217/api/private'
+}
