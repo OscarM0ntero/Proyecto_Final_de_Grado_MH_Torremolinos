4d836c81c2f9b47c1f9c58414fbc188833c93e50
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-11 02:44:01 +0200

    Added languaje selector

--- src/app/web/pages/layout/layout.component.ts
+++ src/app/web/pages/layout/layout.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 //import { User } from 'src/app/auth/interfaces/user.interface';
 //import { AuthService } from 'src/app/auth/services/auth.service';
 
@@ -7,19 +8,36 @@ import { Router } from '@angular/router';
 	selector: 'app-layout',
 	standalone: false,
 	templateUrl: './layout.component.html',
-	styleUrls: ['./layout.component.scss']
+	styleUrls: ['./layout.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 
 export class LayoutComponent {
 
 	currentYear = new Date().getFullYear();
+	mobileMenuOpen = false;
+
+	idiomas: any[] = [
+		{ codigo: 'es', nombre: 'Español', bandera: 'assets/flags/spain.png' },
+		{ codigo: 'en', nombre: 'English', bandera: 'assets/flags/greatBritain.png' },
+		{ codigo: 'de', nombre: 'Deutsch', bandera: 'assets/flags/germany.png' },
+		{ codigo: 'no', nombre: 'Norsk', bandera: 'assets/flags/norway.png' }
+	];
+
+	selectedLang = 'es';
 
 	constructor(
 		private router: Router,
+		private translate: TranslateService
 		//private authService: AuthService
-	) { }
+	) {
+		this.selectedLang = this.translate.currentLang || 'es';
+	}
 
-	mobileMenuOpen = false;
+	cambiarIdioma(lang: string): void {
+		this.selectedLang = lang;
+		this.translate.use(lang);
+	}
 
 	toggleMobileMenu() {
 		this.mobileMenuOpen = !this.mobileMenuOpen;
@@ -37,6 +55,12 @@ export class LayoutComponent {
 	//get user(): string | null {
 	//	return localStorage.getItem('nombre_publico');
 	//}
+
+	get banderaActual(): string {
+		const lang = this.idiomas.find(i => i.codigo === this.selectedLang);
+		return lang ? lang.bandera : '';
+	}
+
 
 	get user(): string {
 		return "User";
