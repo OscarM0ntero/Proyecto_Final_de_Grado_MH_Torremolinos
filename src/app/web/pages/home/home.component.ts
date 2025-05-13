65f7af63e2b89d45e97fdaafdadda4985029868b
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-13 12:31:51 +0200

    Home getting content text from DB from endpoints

--- src/app/web/pages/home/home.component.ts
+++ src/app/web/pages/home/home.component.ts
@@ -1,28 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ContenidoWebService } from '../../../services/contenido-web.service';
+import { Contenido } from '../../../services/contenido.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ContenidoBaseComponent } from '../../../shared/contenido-base.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  host: { ngSkipHydration: 'true' },      // Permite la funcion autoPlay del p-gallery funcionar bien
+  host: { ngSkipHydration: 'true' },
 })
-export class HomeComponent {
-  images: any[] = [
-    {
-      itemImageSrc: 'assets/IMG_01.jpg',
-      alt: 'Imagen 1',
-      title: 'Foto 1'
-    },
-    {
-      itemImageSrc: 'assets/IMG_05.jpg',
-      alt: 'Imagen 2',
-      title: 'Foto 2'
-    },
-    {
-      itemImageSrc: 'assets/IMG_09.jpg',
-      alt: 'Imagen 3',
-      title: 'Foto 3'
-    }
-  ];
+export class HomeComponent extends ContenidoBaseComponent {
+
+  itemLema = '';
+  itemCamaDoble = '';
+  itemCamasInd = '';
+  itemCuna = '';
+  itemApartado1: any;
+  itemApartado2: any;
+
+  constructor(
+    contenidoWeb: ContenidoWebService,
+    translate: TranslateService
+  ) {
+    super(contenidoWeb, translate, 'home');
+  }
+
+  protected actualizarContenido(): void {
+    this.itemLema = this.getTexto(1);
+    this.itemCamaDoble = this.getTexto(2);
+    this.itemCamasInd = this.getTexto(3);
+    this.itemCuna = this.getTexto(4);
+    this.itemApartado1 = this.getContenido(5);
+    this.itemApartado2 = this.getContenido(6);
+  }
 }
