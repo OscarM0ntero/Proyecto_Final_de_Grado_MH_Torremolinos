5d850bc74376781ec3faa45f07a28d62a73e846f
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-25 01:33:36 +0200

    Added cliente routes

--- src/app/web/web-routing.module.ts
+++ src/app/web/web-routing.module.ts
@@ -10,22 +10,43 @@ import { LoginComponent } from './pages/login/login.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { ApartmentComponent } from './pages/apartment/apartment.component';
 
+import { PublicGuard } from '../guards/public.guard';
+import { AdminGuard } from '../guards/admin.guard';
+import { ClientGuard } from '../guards/client.guard';
+import { ClientComponent } from './pages/client/client.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { ClientAccountComponent } from './pages/client/components/client-account/client-account.component';
+import { ClientBookingsComponent } from './pages/client/components/client-bookings/client-bookings.component';
+
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [PublicGuard],
     children: [
-      { path: '', component: HomeComponent},
-      { path: 'apartamento', component: ApartmentComponent},
-      { path: 'galeria', component: GalleryComponent},
-      { path: 'localizacion', component: LocationComponent},
-      { path: 'reservar', component: BookingComponent},
-      { path: 'contacto', component: ContactComponent},
-      { path: 'iniciar-sesion', component: LoginComponent},
-      { path: '**', component: Error404Component}
+      { path: '', component: HomeComponent, canActivate: [PublicGuard] },
+      { path: 'apartamento', component: ApartmentComponent, canActivate: [PublicGuard] },
+      { path: 'galeria', component: GalleryComponent, canActivate: [PublicGuard] },
+      { path: 'localizacion', component: LocationComponent, canActivate: [PublicGuard] },
+      { path: 'contacto', component: ContactComponent, canActivate: [PublicGuard] },
+      { path: 'reservar', component: BookingComponent, canActivate: [PublicGuard] },
+      { path: 'iniciar-sesion', component: LoginComponent, canActivate: [PublicGuard] },
+      {
+        path: 'cliente',
+        component: ClientComponent,
+        canActivate: [ClientGuard],
+        children: [
+          { path: '', redirectTo: 'cuenta', pathMatch: 'full' },
+          { path: 'cuenta', component: ClientAccountComponent },
+          { path: 'reservas', component: ClientBookingsComponent },
+        ]
+      },
+      { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] },
+      { path: '**', component: Error404Component }
     ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
