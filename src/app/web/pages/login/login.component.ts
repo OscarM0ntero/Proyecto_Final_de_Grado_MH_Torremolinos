9ddd24b9de1d5b5c6bda16485b730eefe08631bc
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-25 01:34:41 +0200

    Modified login, working perfectly

--- src/app/web/pages/login/login.component.ts
+++ src/app/web/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email = '';
+  contrasena = '';
+  error = '';
 
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private auth: AuthService,
+    private snackBar: MatSnackBar
+
+  ) { }
+
+  login(): void {
+    this.http.post<{ token: string }>('/api/login', {
+      email: this.email,
+      contrasena: this.contrasena
+    }).subscribe({
+      next: res => {
+        localStorage.setItem('token', res.token);
+
+        const rol = this.auth.getRol();
+        if (rol === "administrador") {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/cliente']);
+        }
+      },
+      error: () => {
+        this.snackBar.open('Correo o contraseña incorrectos', undefined, {
+          duration: 3000,
+          panelClass: ['snackbar-error']
+        });
+      }
+
+    });
+  }
 }
