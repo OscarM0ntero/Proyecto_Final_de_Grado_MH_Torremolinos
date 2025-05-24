189fced0dd8775fee21073b625b273c61da1e441
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-25 01:35:49 +0200

    Added captcha key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
 	webUrl: 'https://www.mhtorremolinos.com/',
 	backgroundUrl: 'https://image.tmdb.org/t/p/original',
 	imageUrl: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2',
-	baseUrl: 'http://79.72.63.217/api/private'
+	baseUrl: 'http://79.72.63.217/api/private',
+	recaptchaSiteKey: '6Le8ZkArAAAAAJjpI5mUh9vt2klOl3Fm0yTdq6jf'
+
 }
