7e91cf06f28daaf894c86b1fd1c327c74297ad58
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-25 01:35:49 +0200

    Added captcha key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,8 @@ export const environment = {
 	webUrl: 'http://localhost:4200/',
 	backgroundUrl: 'https://image.tmdb.org/t/p/original',
 	imageUrl: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2',
-	baseUrl: 'http://79.72.63.217/api/private'
+	baseUrl: 'http://79.72.63.217/api/private',
+	recaptchaSiteKey: '6Le8ZkArAAAAAJjpI5mUh9vt2klOl3Fm0yTdq6jf'
+
+	//https://ical.booking.com/v1/export?t=3e421713-fa23-4d20-8839-80b5a255be7c
 }
