7ed47ded9adf59fb6ffead86a3643d30074f0184
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-05-25 01:32:01 +0200

    Booking component working

--- src/app/web/pages/booking/components/disponibilidad/dialogs/reserva-confirmada.component.ts
+++ src/app/web/pages/booking/components/disponibilidad/dialogs/reserva-confirmada.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../../../material/material.module';
+
+@Component({
+  selector: 'app-reserva-confirmada',
+  template: `
+      <div style="padding: 30px 30px 10px 30px; text-align: center; width: 500px">
+      <h2>Reserva enviada</h2>
+      <p style="margin: 0">Tu solicitud de reserva ha sido enviada. Para finalizar esta reserva deberá <u>realizar una transferencia</u>.<br>Revise su correo para más información.</p>
+      <mat-dialog-actions align="center">
+      <button mat-button mat-dialog-close>Cerrar</button>
+      </mat-dialog-actions>
+      </div>
+  `,
+      imports: [MaterialModule]
+})
+export class ReservaConfirmadaComponent { }
+
+/*
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-reserva-confirmada',
@@ -10,3 +29,4 @@ import { Component } from '@angular/core';
   `
 })
 export class ReservaConfirmadaComponent { }
+*/
--- src/app/web/pages/booking/components/disponibilidad/dialogs/reserva-requiere-login.component.ts
+++ src/app/web/pages/booking/components/disponibilidad/dialogs/reserva-requiere-login.component.ts
@@ -1,5 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MaterialModule } from '../../../../../../material/material.module';
+
+@Component({
+    selector: 'app-reserva-requiere-login',
+    template: `
+        <div style="padding: 30px 30px 10px 30px; text-align: center">
+        <h2>Correo ya registrado</h2>
+        <p style="margin: 0">Este correo ya está registrado.<br>Inicia sesión para continuar con tu reserva.</p>
+        <mat-dialog-actions align="center">
+        <button mat-button mat-dialog-close>Cerrar</button>
+        <button mat-raised-button mat-dialog-close (click)="irALogin()">Ir a inicio de sesión</button>
+        </mat-dialog-actions>
+        </div>
+    `,
+        imports: [MaterialModule]
+})
+export class ReservaRequiereLoginComponent {
+    constructor(private router: Router) { }
+
+    irALogin() {
+        this.router.navigate(['/iniciar-sesion']);
+    }
+}
+
+
+/*
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -23,3 +52,4 @@ export class ReservaRequiereLoginComponent {
         this.router.navigate(['/iniciar-sesion']);
     }
 }
+*/
