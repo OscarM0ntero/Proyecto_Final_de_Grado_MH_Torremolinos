ff50af56457656f35d334d0350114ff00bef5fa0
Oscar Montero <94902365+OscarM0ntero@users.noreply.github.com>
2025-06-01 18:31:51 +0200

    Admin calendar manager basic

---
+++ src/app/web/pages/admin/components/admin-calendar-manager/calendar-colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+    disponible: {
+        primary: 'var(--color-neutral)',
+        secondary: 'var(--color-neutral)'
+    },
+    reservada: {
+        primary: 'var(--color-tertiary)',
+        secondary: 'var(--color-tertiary)'
+    },
+    cerrado: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-primary)'
+    },
+    booking: {
+        primary: 'var(--color-booking)',
+        secondary: 'var(--color-booking)'
+    },
+    airbnb: {
+        primary: 'var(--color-airbnb)',
+        secondary: 'var(--color-airbnb)'
+    }
+};
